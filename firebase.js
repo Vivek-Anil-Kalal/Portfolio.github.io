90de5ba3c48b579338277b0e1ed5ba158d0a8b83
Vivek Kalal <vivekkalal20@gmail.com>
2022-07-26 00:24:37 +0530

    Success message gone after 4 second delay added

--- firebase.js
+++ firebase.js
@@ -27,6 +27,9 @@ import { getDatabase, ref, set, child, update, remove }
 const sendBtn = document.getElementById('contactForm');
 sendBtn.addEventListener('submit', writeUserData);
 
+const sendReviewBtn = document.getElementById('sendReveiw');
+sendReviewBtn.addEventListener('submit', writeUserReview);
+
 function writeUserData(e) {
 
   e.preventDefault();
@@ -39,7 +42,6 @@ function writeUserData(e) {
 
   // ============================================================
 
-
   const db = getDatabase();
 
   set(ref(db, 'users/' + name + date.getTime().toString().substring()), {
@@ -51,7 +53,7 @@ function writeUserData(e) {
   const contactForm = document.getElementById('contactForm');
   contactForm.classList.toggle('fadeAway');
   contactForm.parentNode.removeChild(contactForm);
-  console.log(contactForm.parentNode);                        
+  // console.log(contactForm.parentNode);
 
   const msgSentDiv = document.createElement('div')
   msgSentDiv.innerHTML = `<div class="msgSent">
@@ -59,6 +61,42 @@ function writeUserData(e) {
                             <img src="MyAssets/Contact/sent.gif" alt="Message Sent">
                           </div>`
   document.getElementById('formContainer').appendChild(msgSentDiv)
+
+  setTimeout(() => {
+    msgSentDiv.remove();
+    const formContainer = document.getElementById('formContainer');
+    formContainer.innerHTML = `<form class="contactForm" id="contactForm" action="javascript:writeUserData()" method="post">
+                                  <h3>Send Message</h3>
+                                  <div class="inputBox">
+                                      <input type="text" name="" placeholder="Name" id="nameID" required>
+                                  </div>
+                                  <div class="inputBox">
+                                      <input type="email" name="" placeholder="Email" id="emailID" required>
+                                  </div>
+                                  <div class="inputBox">
+                                      <textarea placeholder="Type Your Message" id="msgID" required></textarea>
+                                  </div>
+                                  <div class="inputBox">
+                                      <input type="submit" value="Send" id="sendBtn">
+                                  </div>
+                              </form>`
+  }, 4000);
+}
+
+//  Review Firebase
+
+function writeUserReview(e) {
+  e.preventDefault();
+
+  const date = new Date();
+
+  const db = getDatabase();
+
+  const name = getSubmittedValues('name');
+  const designation = getSubmittedValues('designation');
+  const review = getSubmittedValues('review');
+
+
 }
 
 function getSubmittedValues(id) {
